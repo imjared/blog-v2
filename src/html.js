93b76102dc3cf04836b7eab40e2f0b7ee00cc9c2
Jared Laser <laserjn@gmail.com>
2019-03-24 10:44:47 -0600

    use secure font link

--- src/html.js
+++ src/html.js
@@ -12,7 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link href="/favicon.png" rel="icon" />
-        <link href="http://fonts.googleapis.com/css?family=Monda|PT+Serif:regular,italic,bold|PT+Sans" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Monda|PT+Serif:regular,italic,bold|PT+Sans" rel="stylesheet" type="text/css" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
